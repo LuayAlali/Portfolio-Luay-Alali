93c734f261343212b86eef116652bf33426f24ba
Luay Alali <luayalali@gmail.com>
2020-09-27 11:14:30 -0700

    click event added to all container; closing open dropdown navbar.

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// jQuery
+
+$('.nav-link').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.contact-link').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.jumbotron').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.container-fluid').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
