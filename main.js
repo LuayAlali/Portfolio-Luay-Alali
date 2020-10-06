d132b0d71d6eda756f1f2af936d78aa38bffa046
Luay Alali <luayalali@gmail.com>
2020-10-06 16:02:40 -0700

    5 projects added to html

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 // jQuery
 
+// navbar
+
 $('.nav-link').on('click',function() {
     $('.navbar-collapse').collapse('hide');
   });
@@ -19,4 +21,15 @@ $('.nav-link').on('click',function() {
 
   $('.bottom-navbar').on('click',function() {
     $('.navbar-collapse').collapse('hide');
+  });
+
+  // cards
+
+
+  $('.container-fluid').on('click',function() {
+    $('.card-1-collapse').collapse('hide');
+  });
+
+  $('.navbar').on('click',function() {
+    $('.card-1-collapse').collapse('hide');
   });
